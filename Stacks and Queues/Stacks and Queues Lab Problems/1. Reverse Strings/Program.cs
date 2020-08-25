4df9c278e4fda9f7ee284a61a52da09d3b4cb3e6
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-25 10:59:16 +0300

    update

---
+++ Stacks and Queues/Stacks and Queues Lab Problems/1. Reverse Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var myStack = new Stack<char>(input);
+
+            while (myStack.Any())
+            {
+                Console.Write(myStack.Pop());
+            }
+        }
+    }
+}
