cb101e6266cd1a3a9072ac912eab54b1962d243d
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-25 15:27:11 +0300

    update

---
+++ Stacks and Queues/Stacks and Queues Lab Problems/7. Hot Potato/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._Hot_Potato
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<string>(input);
+            int currIndex = 1;
+
+            while (queue.Count > 1)
+            {
+                var currName = queue.Dequeue();
+
+                if (currIndex == n)
+                {
+                    Console.WriteLine($"Removed {currName}");
+                    currIndex = 0;
+                }
+                else
+                {
+                    queue.Enqueue(currName);
+                }
+
+                currIndex++;
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
