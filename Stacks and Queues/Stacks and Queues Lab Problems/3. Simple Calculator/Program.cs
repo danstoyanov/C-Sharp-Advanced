a66f71bd3893e6f25b0b31de5141a53e908d4240
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-25 13:23:55 +0300

    update

---
+++ Stacks and Queues/Stacks and Queues Lab Problems/3. Simple Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            var stack = new Stack<string>(input.Reverse());
+
+            while (stack.Count > 1)
+            {
+                var firstNum = int.Parse(stack.Pop());
+                var symbol = stack.Pop();
+                var secondNum = int.Parse(stack.Pop());
+
+                var result = 0;
+
+                switch (symbol)
+                {
+                    case "+":
+                        result = firstNum + secondNum;
+                        break;
+                    case "-":
+                        result = firstNum - secondNum;
+                        break;
+                }
+
+                stack.Push(result.ToString());
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
