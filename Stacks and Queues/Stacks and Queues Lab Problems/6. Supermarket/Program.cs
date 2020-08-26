e8d32268d13e86e17280c148fa003efe9d87d907
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-26 17:52:50 +0300

    update

---
+++ Stacks and Queues/Stacks and Queues Lab Problems/6. Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Supermarket
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = string.Empty;
+            var namesList = new Queue<string>();
+
+            while ((name = Console.ReadLine()) != "End")
+            {
+                if (name.Contains("Paid"))
+                {
+                    while (namesList.Any())
+                    {
+                        Console.WriteLine(namesList.Dequeue());
+                    }
+                }
+                else
+                {
+                    namesList.Enqueue(name);
+                }
+            }
+
+            Console.WriteLine($"{namesList.Count} people remaining.");
+        }
+    }
+}
