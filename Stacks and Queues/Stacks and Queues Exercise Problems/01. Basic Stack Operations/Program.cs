b2119954d2e61968d653b621318cca1244c3ca39
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-27 12:47:29 +0300

    update

---
+++ Stacks and Queues/Stacks and Queues Exercise Problems/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main()
+        {
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int numsOfElements = nums[0];
+            int toPop = nums[1];
+            int numToFind = nums[2];
+
+            var stackNums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < stackNums.Length; i++)
+            {
+                stack.Push(stackNums[i]);
+            }
+
+            for (int i = 1; i <= toPop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(numToFind))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+        }
+    }
+}
