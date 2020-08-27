c35e1bc80bc5ccc7b03a68457fe295a7ac34a779
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-27 13:08:54 +0300

    update

---
+++ Stacks and Queues/Stacks and Queues Exercise Problems/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main()
+        {
+            var elements = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int numsCount = elements[0];
+            int removeCount = elements[1];
+            int numToFind = elements[2];
+
+            var queueElements = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var queue = new Queue<int>(queueElements);
+
+            for (int i = 1; i <= removeCount; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(numToFind))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (queue.Count == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+        }
+    }
+}
