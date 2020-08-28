271ffbfc954ace6b64094d72beaba8f4681e3922
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-08-28 10:48:12 +0300

    update

---
+++ Stacks and Queues/Stacks and Queues Exercise Problems/04. Fast Food/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace _04._Fast_Food
+{
+    class Program
+    {
+        static void Main()
+        {
+            int food = int.Parse(Console.ReadLine());
+            var ordersArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var orders = new Queue<int>(ordersArr);
+            Console.WriteLine(orders.Max());
+
+            for (int i = 0; i < ordersArr.Length; i++)
+            {
+                var currOrder = orders.Peek();
+
+                if (currOrder <= food)
+                {
+                    food -= orders.Dequeue();
+                }
+            }
+
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                var arr = orders.ToArray();
+                Console.WriteLine("Orders left: " + string.Join(" ", arr));
+            }
+        }
+    }
+}
---
+++ Stacks and Queues/Stacks and Queues Exercise Problems/05. Fashion Boutique/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int capacity = int.Parse(Console.ReadLine());
+            var clothes = new Stack<int>(input.Reverse());
+            int sum = 0;
+            int boxes = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                sum += input[i];
+
+                if (sum <= capacity)
+                {
+                    clothes.Pop();
+                }
+                else
+                {
+                    boxes++;
+                    sum = input[i];
+                }
+            }
+
+            if (sum != 0)
+            {
+                Console.WriteLine(boxes += 1);
+            }
+            else
+            {
+                Console.WriteLine(boxes);
+            }
+        }
+    }
+}
