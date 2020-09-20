9caf02f921424f33d4d0f0c574599821842717ef
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-20 15:01:29 +0300

    update

---
+++ Multidimensional Arrays/Multidimensional Arrays Lab Problems/5. Square With Maximum Sum/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Square_With_Maximum_Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            var size = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            var matrix = new int[size[0], size[1]];
+            string bigNumbers = string.Empty;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var currRow = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int maxSumRow = -1;
+            int maxSumCol = -1;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int sum = matrix[row, col];
+                    sum += matrix[row, col + 1];
+                    sum += matrix[row + 1, col];
+                    sum += matrix[row + 1, col + 1];
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxSumCol = col;
+                        maxSumRow = row;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{matrix[maxSumRow, maxSumCol]} {matrix[maxSumRow, maxSumCol + 1]}");
+            Console.WriteLine($"{matrix[maxSumRow + 1, maxSumCol]} {matrix[maxSumRow + 1, maxSumCol + 1]}");
+            Console.WriteLine(maxSum);
+        }
+    }
+}
