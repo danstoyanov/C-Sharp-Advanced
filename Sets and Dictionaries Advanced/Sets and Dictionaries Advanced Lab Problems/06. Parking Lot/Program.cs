6b30a0d494c399449fccb3298947e49514e8c7e3
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-28 12:41:18 +0300

    update

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced Lab Problems/06. Parking Lot/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Parking_Lot
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cars = new HashSet<string>();
+            ParkingCheck(cars);
+            PrintCarsNums(cars);
+        }
+
+        public static void PrintCarsNums(HashSet<string> cars)
+        {
+            if (cars.Count() != 0)
+            {
+                foreach (var number in cars)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+
+        public static void ParkingCheck(HashSet<string> cars)
+        {
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var splited = command
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                var currComm = splited[0];
+                var number = splited[1];
+
+                switch (currComm)
+                {
+                    case "IN":
+                        cars.Add(number);
+                        break;
+
+                    case "OUT":
+                        cars.Remove(number);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
