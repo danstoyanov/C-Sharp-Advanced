4681c302e97b8ab5d59a5c03df68d7d10ab2fcb1
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-28 15:40:37 +0300

    update

--- Sets and Dictionaries Advanced/Sets and Dictionaries Advanced Exercise Problems/01. Unique Usernames/Program.cs
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced Exercise Problems/01. Unique Usernames/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _01._Unique_Usernames
 {
@@ -6,7 +7,19 @@ namespace _01._Unique_Usernames
     {
         static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+            var names = new HashSet<string>();
 
+            for (int name = 1; name <= n; name++)
+            {
+                var currName = Console.ReadLine();
+                names.Add(currName);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
