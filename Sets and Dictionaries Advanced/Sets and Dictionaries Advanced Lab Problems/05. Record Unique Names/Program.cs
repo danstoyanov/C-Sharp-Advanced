bddf413b6f640e202e8784a7aa508dcf624040db
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-28 12:20:27 +0300

    update

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced Lab Problems/05. Record Unique Names/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var names = new HashSet<string>();
+
+            NamesInput(n, names);
+            NamesPrint(names);
+        }
+
+        public static void NamesPrint(HashSet<string> names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public static void NamesInput(int n, HashSet<string> names)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                var nameInput = Console.ReadLine();
+                names.Add(nameInput);
+            }
+        }
+    }
+}
