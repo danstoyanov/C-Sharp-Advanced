a9f8386e441c304c26d6fffb687da42685401edc
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-28 11:58:25 +0300

    update

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced Lab Problems/04. Cities by Continent and Country/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace _04._Cities_by_Continent_and_Country
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            InputInfo(count, dict);
+            PrintInfo(dict);
+        }
+
+        public static void PrintInfo(Dictionary<string, Dictionary<string, List<string>>> dict)
+        {
+            foreach (var (key, value) in dict)
+            {
+                Console.WriteLine($"{key}:");
+                foreach (var pair in value)
+                {
+                    Console.WriteLine($"  {pair.Key} -> {string.Join(", ", pair.Value)}");
+                }
+            }
+        }
+
+        public static void InputInfo(int count, Dictionary<string, Dictionary<string, List<string>>> dict)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+                var continent = input[0];
+                var country = input[1];
+                var city = input[2];
+
+                if (!dict.ContainsKey(continent))
+                {
+                    dict.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!dict[continent].ContainsKey(country))
+                {
+                    dict[continent].Add(country, new List<string>());
+                }
+
+                dict[continent][country].Add(city);
+            }
+        }
+    }
+}
