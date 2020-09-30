34b1ad55d21f86f8711479f282dbac3e377c54cb
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-30 13:12:02 +0300

    update

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced Exercise Problems/04. Even Times/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var counts = new Dictionary<int, int>();
+            FillNums(n, counts);
+            AppearsNumPrint(counts);
+        }
+
+        public static void AppearsNumPrint(Dictionary<int, int> counts)
+        {
+            var appears = counts.Where(x => x.Value % 2 == 0).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var count in appears)
+            {
+                Console.WriteLine(count.Key);
+            }
+        }
+
+        public static void FillNums(int n, Dictionary<int, int> counts)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                int currNum = int.Parse(Console.ReadLine());
+
+                if (!counts.ContainsKey(currNum))
+                {
+                    counts.Add(currNum, 0);
+                }
+
+                counts[currNum]++;
+            }
+        }
+    }
+}
