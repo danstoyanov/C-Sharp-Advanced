db7e269e6de9ca217c63a75c91fbe79bf8e381dc
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-09-30 13:11:21 +0300

    update

---
+++ Sets and Dictionaries Advanced/Sets and Dictionaries Advanced Exercise Problems/03. Periodic Table/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var set = new HashSet<string>();
+            SetFill(n, set);
+            SetPrint(set);
+        }
+
+        public static void SetPrint(HashSet<string> set)
+        {
+            var sortedSet = set.OrderBy(x => x).ToList();
+            foreach (var element in sortedSet)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        public static void SetFill(int n, HashSet<string> set)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                var element = Console.ReadLine().Split();
+
+                for (int j = 0; j < element.Length; j++)
+                {
+                    set.Add(element[j]);
+                }
+            }
+        }
+    }
+}
