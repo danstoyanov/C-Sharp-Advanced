96085e9618973e0fcd380e5e0d1973e0ab5feeb1
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-02 17:44:57 +0300

    update

---
+++ Streams Files and Directories/Streams Files and Directories Lab Problems/02. Line Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _02._Line_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            using var reader = new StreamReader("../../../NumFile.txt");
+            int count = 0;
+
+            while (true)
+            {
+                count++;
+                var line = reader.ReadLine();
+
+                if (line == null)
+                {
+                    break;
+                }
+
+                Console.WriteLine($"{count}. {line}");
+            }
+        }
+    }
+}
