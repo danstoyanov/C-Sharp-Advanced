5f9b78941ac009e2c7a5bd9de6f18586b06f6141
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-04 19:15:30 +0300

    update

---
+++ Functional Programming/Functional Programming Lab Problems/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<string, bool> isUpper = curr => char.IsUpper(curr[0]);
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(isUpper)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
