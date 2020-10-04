122d95dad5ca851a5ef2dceba30f331a3cd8dc14
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-04 19:15:07 +0300

    update

---
+++ Functional Programming/Functional Programming Lab Problems/02. Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<string, int> numParse = int.Parse;
+            var nums = Console.ReadLine()
+                .Split(", ")
+                .Select(numParse)
+                .ToList();
+
+            Console.WriteLine(nums.Count);
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
