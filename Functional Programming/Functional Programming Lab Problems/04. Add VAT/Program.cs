80737df4d4ab34c36a37ac72d5590cb8a2eab4ee
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-04 19:54:49 +0300

    update

---
+++ Functional Programming/Functional Programming Lab Problems/04. Add VAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<string, double> doubleType = double.Parse;
+            Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(doubleType)
+                .Select(n => n = ((n + (n * 0.20))))
+                .Select(n => new string($"{n:f2}"))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
