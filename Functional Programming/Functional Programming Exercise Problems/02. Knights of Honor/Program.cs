0539812b7d1eb7db2e648e3a4bfbcbdb578f71a8
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-05 15:13:16 +0300

    update

---
+++ Functional Programming/Functional Programming Exercise Problems/02. Knights of Honor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine()
+                .Split()
+                .Select(x => "Sir " + x)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
