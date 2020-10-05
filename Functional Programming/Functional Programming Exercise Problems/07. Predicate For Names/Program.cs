975388e66d52ef77c5a291d889b1f6e690b057f5
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-05 21:56:00 +0300

    update

---
+++ Functional Programming/Functional Programming Exercise Problems/07. Predicate For Names/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    class Program
+    {
+        static void Main()
+        {
+            int nameLenght = int.Parse(Console.ReadLine());
+            Func<string, bool> isLess = name => name.Length <= nameLenght;
+            Console.ReadLine()
+                .Split()
+                .Where(isLess)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
