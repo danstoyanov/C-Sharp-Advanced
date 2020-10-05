f7c2d3bb5b472bdf9eda0af1167a9fcdadee8808
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-05 15:09:19 +0300

    update

---
+++ Functional Programming/Functional Programming Exercise Problems/01. Action Print/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Action_Print
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine()
+                .Split()
+                .Select(x => x)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
