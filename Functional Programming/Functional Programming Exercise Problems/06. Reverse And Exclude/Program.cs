488b812640f9aacf6defaff01a8a64d721c4e520
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-05 20:59:06 +0300

    update

---
+++ Functional Programming/Functional Programming Exercise Problems/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main()
+        {
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+            Predicate<int> disivisibleFunc = x => x % n != 0;
+            Console.WriteLine(string.Join(" ", nums.FindAll(disivisibleFunc)));
+        }
+    }
+}
