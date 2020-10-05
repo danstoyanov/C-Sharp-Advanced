bb49d01ec948119adcc37fb29ace75853e5c9ce3
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-05 18:10:27 +0300

    update

---
+++ Functional Programming/Functional Programming Exercise Problems/03. Custom Min Function/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Func<int[], int> minValue = minNum => arr.Min();
+            Console.WriteLine(minValue(arr));
+        }
+    }
+}
