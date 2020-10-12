718b6897fcda0ecd27236091f6ba65358c5d5fb6
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-12 15:37:04 +0300

    update

---
+++ Defining Classes/Defining Classes Lab Problems/02. Car Extension/Car.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSecond
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive (double distance)
+        {
+            double fuel = FuelQuantity - (distance * FuelConsumption);
+
+            if (fuel > 0)
+            {
+                FuelQuantity = fuel;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:F2}L");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Defining Classes/Defining Classes Lab Problems/02. Car Extension/Program.cs
@@ -0,0 +1,21 @@
+﻿using CarSecond;
+using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(2000);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
