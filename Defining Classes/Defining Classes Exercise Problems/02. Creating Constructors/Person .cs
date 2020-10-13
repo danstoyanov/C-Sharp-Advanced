dc0d2d1139c5c702b24ceb6785f8a4704f844054
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-13 16:20:58 +0300

    update

---
+++ Defining Classes/Defining Classes Exercise Problems/02. Creating Constructors/Person .cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person() // No arguments ! <== Constructor !
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age) : this()// Argument with integer
+        {
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name 
+        {
+            get { return this.Name = name; }
+            set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return this.Age = age; }
+            set { this.age = value; }
+        }
+    }
+}
