eee024434ca1a56aa90283ab7302017e1c6659cd
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-14 17:37:15 +0300

    update

---
+++ C# Advanced - Exams/C Sharp Advanced Retake Exam - 19 August 2020/01. Flower Wreaths/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Flower_Wreaths
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] lilies, roses;
+            ReadFromConsole(out lilies, out roses);
+
+            var stack = new Stack<int>(lilies);
+            var queue = new Queue<int>(roses);
+
+            int wreathCount = 0;
+            int wreathLeft = 0;
+
+            while (stack.Count != 0  && queue.Count != 0)
+            {
+                int currLilie = stack.Peek();
+                int currRose = queue.Peek();
+
+                if (currLilie + currRose == 15)
+                {
+                    stack.Pop();
+                    queue.Dequeue();
+                    wreathCount++;
+                }
+
+                if (currLilie + currRose > 15)
+                {
+                    while (currLilie + currRose > 15)
+                    {
+                        currLilie -= 2;
+                    }
+
+                    if (stack.Count != 0 && (currLilie + currRose) == 15)
+                    {
+                        stack.Pop();
+                        queue.Dequeue();
+                        wreathCount++;
+                    }
+                }
+
+                if (currLilie + currRose < 15)
+                {
+                    wreathLeft += currLilie + currRose;
+                    stack.Pop();
+                    queue.Dequeue();
+                }
+            }
+
+            if (wreathLeft >= 15)
+            {
+                var leftFlow = wreathLeft / 15;
+                wreathCount += leftFlow;
+            }
+
+            if (wreathCount >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreathCount} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreathCount} wreaths more!");
+            }
+        }
+
+        public static void ReadFromConsole(out int[] lilies, out int[] roses)
+        {
+            lilies = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+            roses = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ Stacks and Queues/Stacks and Queues Exercise Problems/12. Cups and Bottles/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cups = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var cupsQueue = new Queue<int>(cups);
+            
+            var bottles = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var stackBottles = new Stack<int>(bottles);
+
+            int totalWastedWater = 0;
+
+            while (cupsQueue.Count > 0)
+            {
+                var currBottle = stackBottles.Peek();
+                var currCupValue = cupsQueue.Peek();
+                
+                if (currBottle >= currCupValue)
+                {
+                    totalWastedWater += currBottle - currCupValue;
+                    stackBottles.Pop();
+                    cupsQueue.Dequeue();
+                }
+                else // WARNING !!!!!!!!!!!
+                {
+                    currCupValue = cupsQueue.Dequeue();
+                    var result = currCupValue - currBottle;
+                    stackBottles.Pop();
+                    var currCups = cupsQueue.ToList();
+                    currCups.Insert(0, result);
+                    cupsQueue = new Queue<int>(currCups);
+                }
+
+                if (stackBottles.Count <= 0)
+                {
+                    break;
+                }
+            }
+
+            if (stackBottles.Count > 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", stackBottles)}");
+            }
+            else if (cupsQueue.Count > 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cupsQueue)}");
+            }
+
+            Console.WriteLine($"Wasted litters of water: {totalWastedWater}");
+        }
+    }
+}
