5b75956885bbf46e193430a02ae28de1e763af59
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-28 13:15:05 +0200

    update

--- Defining Classes/Defining Classes Exercise Problems/P01_Define_a_Class_Person/StartUp.cs
+++ Defining Classes/Defining Classes Exercise Problems/P01_Define_a_Class_Person/StartUp.cs
@@ -4,9 +4,23 @@ namespace DefiningClasses
 {
     public class StartUp
     {
-        public static void Main()
+        static void Main()
         {
+            var name1 = new Person();
+            name1.Name = "Pesho";
+            name1.Age = 20;
 
+            var name2 = new Person();
+            name2.Name = "Gosho";
+            name2.Age = 18;
+
+            var name3 = new Person();
+            name3.Name = "Stamat";
+            name3.Age = 43;
+
+            Console.WriteLine($"{name1.Name} => {name1.Age}");
+            Console.WriteLine($"{name2.Name} => {name2.Age}");
+            Console.WriteLine($"{name3.Name} => {name3.Age}");
         }
     }
 }
