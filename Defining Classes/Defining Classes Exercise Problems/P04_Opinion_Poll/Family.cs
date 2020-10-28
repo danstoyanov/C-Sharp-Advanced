0f5ce569dfecef459ebbd52eb09451be554ab99b
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-28 13:43:48 +0200

    update

---
+++ Defining Classes/Defining Classes Exercise Problems/P04_Opinion_Poll/Family.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private HashSet<Person> members;
+
+        public Family()
+        {
+            this.members = new HashSet<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            var oldestPerson = this.members
+                .OrderByDescending(p => p.Age)
+                .FirstOrDefault();
+
+            return oldestPerson;
+        }
+
+        public HashSet<Person> GetAllPeopleAbove30()
+        {
+            return this.members
+                .Where(p => p.Age > 30)
+                .OrderBy(p => p.Name)
+                .ToHashSet();
+        }
+    }
+}
---
+++ Defining Classes/Defining Classes Exercise Problems/P04_Opinion_Poll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var personArgs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var name = personArgs[0];
+                var age = int.Parse(personArgs[1]);
+
+                var person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            var result = family.GetAllPeopleAbove30();
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
