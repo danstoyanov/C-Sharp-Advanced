625b61680f2035b5be3b4bbbbd5f2ea24ac66df9
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-28 13:16:19 +0200

    update

---
+++ Defining Classes/Defining Classes Exercise Problems/P03_Oldest_Family_Member/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var personArgs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var name = personArgs[0];
+                var age = int.Parse(personArgs[1]);
+
+                var person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Console.WriteLine(family.GetOldestMember());
+        }
+    }
+}
