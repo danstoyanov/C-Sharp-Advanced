470559f00ad05d74c4244154f36fec913f1b71fe
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-10-29 12:56:58 +0200

    update

---
+++ Defining Classes/Defining Classes Exercise Problems/P06_Speed_Racing/Car.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace P06_Speed_Racing
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumption;
+        private double travelledDistance;
+
+        public Car(string model)
+        {
+            this.Model = model;
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumption) 
+            :this(model)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+
+        public double FuelAmount
+        {
+            get
+            {
+                return this.fuelAmount;
+            }
+            set
+            {
+                this.fuelAmount = value;
+            }
+        }
+
+        public double FuelConsumption
+        {
+            get
+            {
+                return this.fuelConsumption;
+            }
+            set
+            {
+                this.fuelConsumption = value;
+            }
+        }
+
+        public double TravelledDistance
+        {
+            get
+            {
+                return this.travelledDistance;
+            }
+            set
+            {
+                this.travelledDistance = value;
+            }
+        }
+
+        public static List<Car> Drive(List<Car> cars, 
+            string carModel, double amountKm)
+        {
+            var driveCar = cars.Find(x => x.Model == carModel);
+            var fuelNeed = amountKm * driveCar.FuelConsumption;
+
+            if (fuelNeed > driveCar.FuelAmount)
+            {
+                System.Console.WriteLine($"Insufficient fuel for the drive");
+            }
+            else
+            {
+                driveCar.TravelledDistance += amountKm;
+                driveCar.FuelAmount -= fuelNeed;
+            }
+
+            return cars;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Model} {this.FuelAmount:F2} {this.TravelledDistance:F0}";
+        }
+    }
+}
---
+++ Defining Classes/Defining Classes Exercise Problems/P06_Speed_Racing/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06_Speed_Racing
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var carArgs = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                var model = carArgs[0];
+                var fuelAmount = int.Parse(carArgs[1]);
+                var fuelConsumptionFor1km = double.Parse(carArgs[2]);
+
+                var currCar = new Car(model, fuelAmount, fuelConsumptionFor1km);
+                cars.Add(currCar);
+            }
+
+            var command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                var currComm = command.Split().ToArray();
+                var carModel = currComm[1];
+                var amountKm = double.Parse(currComm[2]);
+
+                Car.Drive(cars, carModel, amountKm);
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+    }
+}
