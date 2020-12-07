8bab820aa37a0fcb6c9b0a3495b01d1df54c8447
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-12-07 19:22:19 +0200

    update

---
+++ C# Advanced - Exams/C Sharp Advanced Retake Exam - 19 August 2020/03. VetClinic/VetClinic/Clinic.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+        private int capacity;
+
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.data = new List<Pet>();
+        }
+
+        public int Capacity
+        {
+            get => this.capacity;
+            set => this.capacity = value;
+        }
+
+        public int Count => this.data.Count;
+
+        public void Add(Pet pet)
+        {
+            if (this.data.Count < capacity)
+            {
+                this.data.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var currPetName = this.data.FirstOrDefault(p => p.Name == name);
+
+            if (currPetName != null)
+            {
+                this.data.Remove(currPetName);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            var currName = this.data.FirstOrDefault(p => p.Name == name && p.Owner == owner);
+
+            return currName;
+        }
+
+        public Pet GetOldestPet()
+        {
+            var maxValue = this.data.Max(p => p.Age);
+            var oldestPet = this.data.FirstOrDefault(p => p.Age == maxValue);
+
+            return oldestPet;
+        }
+
+        public string GetStatistics()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+
+            foreach (var pet in this.data)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
