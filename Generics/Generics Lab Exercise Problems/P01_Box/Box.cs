c823fe0e3020b143cd496c071704e39d5ea40fcd
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-12-13 17:07:33 +0200

    update

---
+++ Generics/Generics Lab Exercise Problems/P01_Box/Box.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> data;
+
+        public Box()
+        {
+            this.data = new Stack<T>();
+        }
+
+        public int Count => this.data.Count;
+
+        public void Add(T item)
+        {
+            this.data.Push(item);
+        }
+        public T Remove()
+        {
+            return this.data.Pop();
+        }
+    }
+}
