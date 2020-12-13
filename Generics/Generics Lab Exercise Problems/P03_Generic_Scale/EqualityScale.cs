831d0d1677d1a19f705d041413206646c475f7ad
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-12-13 17:08:23 +0200

    update

---
+++ Generics/Generics Lab Exercise Problems/P03_Generic_Scale/EqualityScale.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        public T left;
+        public T right;
+
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public bool AreEqual()
+        {
+            return this.left.Equals(this.right);
+        }
+    }
+}
---
+++ Generics/Generics Lab Exercise Problems/P03_Generic_Scale/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var item = new EqualityScale<int>(3, 2);
+            Console.WriteLine(item.AreEqual());
+
+            var seon = new EqualityScale<string>("D", "");
+            Console.WriteLine(seon.AreEqual());
+
+            var charecter = new EqualityScale<char>('D', 'A');
+            Console.WriteLine(charecter.AreEqual());
+        }
+    }
+}
