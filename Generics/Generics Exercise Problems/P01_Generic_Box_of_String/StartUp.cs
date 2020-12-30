4c8c028c7e936c556ed2b6684c5b75b5460e41e9
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-12-30 14:33:48 +0200

    update

---
+++ Generics/Generics Exercise Problems/P01_Generic_Box_of_String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P01_Generic_Box_of_String
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var box = new Box<string>(input);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
