e00927c03915270ffeae9c78dd6675906fbba149
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-12-31 16:16:32 +0200

    update

---
+++ Generics/Generics Exercise Problems/P04_Generic_Swap_Method_Integer/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04_Generic_Swap_Method_Integer
+{
+    public class Box <T>
+    {
+        public List<T> Values { get; set; } = new List<T>();
+
+        public Box(List<T> values)
+        {
+            this.Values = values;
+        }
+
+        public void Swap (int firstValue, int secondValue)
+        {
+            var tempValue = this.Values[firstValue];
+            this.Values[firstValue] = this.Values[secondValue];
+            this.Values[secondValue] = tempValue;
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (T value in this.Values)
+            {
+                stringBuilder.AppendLine($"{value.GetType()}: {value}");
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
---
+++ Generics/Generics Exercise Problems/P04_Generic_Swap_Method_Integer/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Generic_Swap_Method_Integer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var values = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+                values.Add(number);
+            }
+
+            var indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var box = new Box<int>(values);
+            box.Swap(indexes[0], indexes[1]);
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
