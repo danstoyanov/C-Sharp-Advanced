27b3eef4665a334eddb2a8657e1aeab212a45996
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-01 20:56:09 +0200

    update

---
+++ Generics/Generics Exercise Problems/P05_Generic_Count_Method_String/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05_Generic_Count_Method_String
+{
+    public class Box <T> where T : IComparable
+    {
+        public Box(List<T> values)
+        {
+            this.Values = values;
+        }
+
+        public List<T> Values { get; set; } = new List<T>();
+
+        public int CountOfGreaterElements (T currValue)
+        {
+            var count = 0;
+
+            foreach (T value in this.Values)
+            {
+                if (value.CompareTo(currValue) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Generics/Generics Exercise Problems/P05_Generic_Count_Method_String/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05_Generic_Count_Method_String
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var values = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                values.Add(input);
+            }
+
+            var elemetnForComparison = Console.ReadLine();
+            var box = new Box<string>(values);
+
+            Console.WriteLine(box.CountOfGreaterElements(elemetnForComparison));
+        }
+    }
+}
