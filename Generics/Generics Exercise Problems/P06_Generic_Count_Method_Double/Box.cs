2809c2706c883c43c9ef6dc08a199fd390c6bac4
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-01 20:56:48 +0200

    update

---
+++ Generics/Generics Exercise Problems/P06_Generic_Count_Method_Double/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06_Generic_Count_Method_Double
+{
+    public class Box<T> where T : IComparable
+    {
+        public Box(List<T> values)
+        {
+            this.Values = values;
+        }
+
+        public List<T> Values { get; set; } = new List<T>();
+
+        public int ValuesComparison(T currValue)
+        {
+            var count = 0;
+
+            foreach (T value in this.Values)
+            {
+                if (value.CompareTo(currValue) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Generics/Generics Exercise Problems/P06_Generic_Count_Method_Double/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06_Generic_Count_Method_Double
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var values = new List<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var number = double.Parse(Console.ReadLine());
+                values.Add(number);
+            }
+
+            var testNumber = double.Parse(Console.ReadLine());
+            var box = new Box<double>(values);
+
+            Console.WriteLine(box.ValuesComparison(testNumber));
+        }
+    }
+}
