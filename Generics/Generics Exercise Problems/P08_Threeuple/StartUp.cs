1c58e51743f2ca7dc6e65583fd75584b5ec2f487
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-02 15:01:58 +0200

    update

---
+++ Generics/Generics Exercise Problems/P08_Threeuple/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace P08_Threeuple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var firstInput = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            var firstName = firstInput[0];
+            var lastName = firstInput[1];
+            var address = firstInput[2];
+            var town = firstInput[3];
+
+            var secondInput = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            var secondName = secondInput[0];
+            var litersOfbeer = int.Parse(secondInput[1]);
+            var drunkOrNot = secondInput[2] == "drunk" ? true : false;
+
+            var thirdInput = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            var thirdName = thirdInput[0];
+            var balance = double.Parse(thirdInput[1]);
+            var bankName = thirdInput[2];
+
+            var firstInfo = new Threeuple<string, string, string>((firstName + " " + lastName), address, town);
+            var secondInfo = new Threeuple<string, int, bool>(secondName, litersOfbeer, drunkOrNot);
+            var thirdInfo = new Threeuple<string, double, string>(thirdName, balance, bankName);
+
+            Console.WriteLine(firstInfo);
+            Console.WriteLine(secondInfo);
+            Console.WriteLine(thirdInfo);
+        }
+    }
+}
---
+++ Generics/Generics Exercise Problems/P08_Threeuple/Threeuple.cs
@@ -0,0 +1,21 @@
+﻿namespace P08_Threeuple
+{
+    public class Threeuple <T1, T2, T3>
+    {
+        public Threeuple(T1 first, T2 second, T3 third)
+        {
+            this.First = first;
+            this.Second = second;
+            this.Third = third;
+        }
+
+        public T1 First { get; set; }
+        public T2 Second { get; set; }
+        public T3 Third { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.First} -> {this.Second} -> {this.Third}";
+        }
+    }
+}
