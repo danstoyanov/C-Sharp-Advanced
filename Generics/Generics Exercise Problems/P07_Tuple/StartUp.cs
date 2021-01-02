474334d9e91405009892628ee50faab0f0c4b2d8
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-02 15:01:19 +0200

    update

---
+++ Generics/Generics Exercise Problems/P07_Tuple/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var firstInput = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            var name = firstInput[0] + " " + firstInput[1];
+            var city = firstInput[2];
+
+            var secondInput = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            var currName = secondInput[0];
+            var liters = int.Parse(secondInput[1]);
+
+            var thirdInput = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            var integer = int.Parse(thirdInput[0]);
+            var doubleNum = double.Parse(thirdInput[1]);
+
+            var firstInfo = new Tuple<string, string>(name, city);
+            var secondInfo = new Tuple<string, int>(currName, liters);
+            var thirdInfo = new Tuple<int, double>(integer, doubleNum);
+
+            Console.WriteLine(firstInfo);
+            Console.WriteLine(secondInfo);
+            Console.WriteLine(thirdInfo);
+        }
+    }
+}
---
+++ Generics/Generics Exercise Problems/P07_Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuple <TFrist, TSecond>
+    {
+        public Tuple(TFrist item1, TSecond item2)
+        {
+            this.Item1 = item1;
+            this.Item2 = item2;
+        }
+
+        public TFrist Item1 { get; set; }
+        public TSecond Item2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Item1} -> {this.Item2}";
+        }
+    }
+}
