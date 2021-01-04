5edb9c319ed60a82dd903381c691947c4f1b3594
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-04 15:14:11 +0200

    update

---
+++ Iterators and Comparators/Iterators and Comparators Lab Problems/IteratorsAndComparators/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public Book(string title, int year, params string[] autors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Autors = new List<string>(autors);
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+
+        public List<string> Autors { get; set; }
+    }
+}
---
+++ Iterators and Comparators/Iterators and Comparators Lab Problems/IteratorsAndComparators/Library.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            public List<Book> books;
+            public int currentIndex;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => this.books[currentIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose() // NO !
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return ++this.currentIndex < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.currentIndex = -1;
+            }
+        }
+    }
+}
