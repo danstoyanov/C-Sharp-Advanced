eed9b4ec6e6696fd0eb72e2c1982775a7855ec97
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-07 13:30:04 +0200

    update

---
+++ Iterators and Comparators/Iterators and Comparators Exercise Problems/P03_Stack/MyStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+
+        public MyStack(List<T> inputElements)
+        {
+            this.elements = inputElements;
+        }
+        public int Count => this.elements.Count;
+
+        public void Push(T element)
+        {
+            this.elements.Add(element);
+        }
+
+        public void Pop() // ???? 
+        {
+            if (this.Count != 0)
+            {
+                this.elements.RemoveAt(this.elements.Count - 1);
+            }
+            else
+            {
+                Console.WriteLine($"No elements");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.elements.Count - 1; i >= 0; i--)
+            {
+                yield return this.elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators/Iterators and Comparators Exercise Problems/P03_Stack/StartUp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_Stack
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var mainArr = Console.ReadLine()
+                .Split(new string[] { ", ", " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToArray();
+
+            var stack = new MyStack<string>(new List<string>(mainArr));
+            var command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var currComm = command
+                    .Split(new string[] { ", ", " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                switch (currComm[0])
+                {
+                    case "Push":
+                        var element = currComm[1];
+                        stack.Push(element);
+                        break;
+
+                    case "Pop":
+                        stack.Pop();
+                        break;
+                }
+            }
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
+/* 1. We need to create a stack structure 
+ * 2. One collection for input elements ===> OK ! 
+ * 3. Two functions (OOP) ====> OK !
+
+ *  => Pop()
+ *  => Push() == OK !
+
+ *  WARNING !!! => . Keep in mind that the first element, which is popped is the last in the collection
+ * 
+ * 
+ * 
+ */
