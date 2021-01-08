9d409532214a98699fc53ce9a880de4528c27b7c
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-08 12:13:59 +0200

    update

---
+++ Iterators and Comparators/Iterators and Comparators Exercise Problems/P04_Froggy/Lake.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private readonly List<int> stones;
+
+        public Lake(params int[] stonesNums)
+        {
+            this.stones = new List<int>(stonesNums);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Count; i += 2)
+            {
+                yield return stones[i];
+            }
+
+            for (int i = stones.Count - 1; i > 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators/Iterators and Comparators Exercise Problems/P04_Froggy/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04_Froggy
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var array = Console.ReadLine()?
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var stones = new Lake(array);
+            var sortedStones = new List<int>();
+
+            Console.WriteLine(string.Join(", ", stones));
+        }
+    }
+}
